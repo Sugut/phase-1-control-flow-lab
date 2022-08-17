724ae9fa7bf8e9b23ccb0930aac7144418703db9
Sugut <sugutvictorine@gmail.com>
2022-08-17 22:32:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ function scuberGreetingForFeet(distance){
+  if(distance <= 400){
+     return "This one is on me!"
+  }
+  else if(distance > 2500){
+    return "No can do."
+  }
+  else if (distance > 2000){
+    return "I will gladly take your thirty bucks."
+  }
+ }
+  
+ function ternaryCheckCity(city){
+  if (city === "NYC")
+   return "Ok, sounds good."
+   else (city === "Pittsburgh")
+   return "No go."
+ }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+ function switchOnCharmFromTip(tip){
+  switch(tip){
+     case"generous":
+       return "Thank you so much."
+     break;
+      case "not as generous":
+       return "Thank you."
+    break;
+      default:
+       return "Bye."
+    break;
+  }  
+ }
